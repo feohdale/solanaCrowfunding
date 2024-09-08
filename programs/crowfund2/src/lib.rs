638274b911ecdbdd06918cd1e9d25013cd4f5062
feohdale <145969313+feohdale@users.noreply.github.com>
2024-09-08 16:16:24 +0200

    Update lib.rs
    ajout de fermeture auto de cagnotte quand l'utilisateur withdraw

--- programs/crowfund2/src/lib.rs
+++ programs/crowfund2/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 //use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 use anchor_lang::system_program;
-declare_id!("GKyC3AKQt3TgW2nEJaZYUXXiGBjBMsURgsqvJRooZgzr");
+declare_id!("5iNogQnmKqvvJnC2TqTvAimSdmcmGx4uESAF45E52tBh");
 
 #[program]
 pub mod cagnotte2 {
@@ -12,20 +12,22 @@ pub mod cagnotte2 {
         cagnotte.owner = *ctx.accounts.user.key;
         cagnotte.name = name.as_bytes().to_vec();
         cagnotte.amount = 0;
-    // intisalise la cagnotte  avec un montant de 0 PDA a init avec une seed qui attend le nom (string) cagnotte, la clé publique de 
-    // la clé publique de l'utilisateur et la string du nom de la cagnotte.  
+        // intisalise la cagnotte  avec un montant de 0 PDA a init avec une seed qui attend le nom (string) cagnotte, la clé publique de
+        // la clé publique de l'utilisateur et la string du nom de la cagnotte.
+        cagnotte.locked = false;
+        //cagnotte.contributions.push((*ctx.accounts.user.key, 0));
         Ok(())
     }
 
     pub fn initialize_admin(ctx: Context<InitializeAdmin>) -> Result<()> {
         // Vérifier si le compte admin existe déjà donc si le solde est a 0 :)
-        
+
         /*if ctx.accounts.admin_account.to_account_info().lamports() > 0 {
             return Err(ErrorCode::AdminAccountAlreadyExists.into());
         }*/
 
         let admin_account = &mut ctx.accounts.admin_account;
-        // seul l'init du program devient le premier admin 
+        // seul l'init du program devient le premier admin
         admin_account.admins = vec![*ctx.accounts.user.key];
 
         Ok(())
@@ -35,7 +37,7 @@ pub mod cagnotte2 {
         let admin_account = &mut ctx.accounts.admin_account;
 
         // test si admin = le demandeur
-        
+
         if !admin_account.admins.contains(ctx.accounts.user.key) {
             return Err(ErrorCode::Unauthorized.into());
         }
@@ -80,11 +82,9 @@ pub mod cagnotte2 {
     pub fn contribute(ctx: Context<Contribute>, amount: u64) -> Result<()> {
         let cagnotte = &mut ctx.accounts.cagnotte;
 
-
         if cagnotte.locked {
-        return Err(ErrorCode::CagnotteLocked.into());
-    }
-
+            return Err(ErrorCode::CagnotteLocked.into());
+        }
 
         let cpi_context = CpiContext::new(
             ctx.accounts.system_program.to_account_info(),
@@ -96,16 +96,14 @@ pub mod cagnotte2 {
         system_program::transfer(cpi_context, amount)?;
 
         ctx.accounts.cagnotte.amount += amount;
-        ctx.accounts.contribution.amount +=amount;
+        ctx.accounts.contribution.amount += amount;
         Ok(())
     }
 
-        // l'appel a contribute, crée un pda si necessaire, de la forme suivante : 
-        //seeds= [b"contribution", cagnotte.key().as_ref(), user.key().as_ref()],
-        //donc l'appel a contribution se fait par la seed string contribution, 
-        // la public key du pda de la cagnotte que l'on appelle  public key de l'user, 
-
-
+    // l'appel a contribute, crée un pda si necessaire, de la forme suivante :
+    //seeds= [b"contribution", cagnotte.key().as_ref(), user.key().as_ref()],
+    //donc l'appel a contribution se fait par la seed string contribution,
+    // la public key du pda de la cagnotte que l'on appelle  public key de l'user,
 
     pub fn get_balance(ctx: Context<GetBalance>) -> Result<()> {
         let cagnotte = &ctx.accounts.cagnotte;
@@ -118,68 +116,74 @@ pub mod cagnotte2 {
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
         let cagnotte = &mut ctx.accounts.cagnotte;
-        msg!("account to withdraw {} ", cagnotte.key());
+    
         // Vérifier si la cagnotte est verrouillée
         if cagnotte.locked {
-        return Err(ErrorCode::CagnotteLocked.into());
+            return Err(ErrorCode::CagnotteLocked.into());
         }
-        msg!("amount to withdraw {} Lamports", amount);
-        // msg!("account to withdraw {} ", account_address);
-        
-        //verification du owner pour s'assurer que personne d'autre ne puisse recuperer les fonds. 
+    
+        // Vérifier que l'utilisateur est le propriétaire de la cagnotte (pas besoin de mutable ici)
         if cagnotte.owner != *ctx.accounts.user.key {
             return Err(ErrorCode::Unauthorized.into());
         }
-        
-        // verification du montant que  la somme detenue par le pda est au superieur ou égal au montant du amount 
+    
+        // Vérifier que le montant à retirer est disponible
         if cagnotte.amount < amount {
             return Err(ErrorCode::InsufficientFunds.into());
         }
+    
+        // Effectuer le retrait : Diminuer le montant dans la cagnotte
         cagnotte.amount -= amount;
-        **ctx
-            .accounts
-            .cagnotte
-            .to_account_info()
-            .try_borrow_mut_lamports()? -= amount;
-        **ctx
-            .accounts
-            .user
-            .to_account_info()
-            .try_borrow_mut_lamports()? += amount;
+    
+        // Emprunter user de manière mutable pour les lamports seulement ici
+        let user = &mut ctx.accounts.user;
+    
+        // Transférer les lamports du compte de la cagnotte vers le compte de l'utilisateur
+        **cagnotte.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
+    
+        // La macro #[account(mut, close = user)] dans le contexte s'assurera que
+        // si le solde de la cagnotte est à zéro, le compte sera fermé
+        Ok(())
+    }
+    
+    
+    
+    pub fn lock_cagnotte(ctx: Context<ManageCagnotteLock>) -> Result<()> {
+        let cagnotte = &mut ctx.accounts.cagnotte;
+
+        let admin_account = &ctx.accounts.admin_account;
+
+        // Vérifier si l'utilisateur est bien un administrateur
+        if !admin_account.admins.contains(ctx.accounts.user.key) {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+        cagnotte.locked = true;
+        msg!(
+            "La cagnotte {} a été verrouillée.",
+            String::from_utf8_lossy(&cagnotte.name)
+        );
 
         Ok(())
     }
-    pub fn lock_cagnotte(ctx: Context<ManageCagnotteLock>) -> Result<()> {
-    let cagnotte = &mut ctx.accounts.cagnotte;
 
-    let admin_account = &ctx.accounts.admin_account;
+    pub fn unlock_cagnotte(ctx: Context<ManageCagnotteLock>) -> Result<()> {
+        let cagnotte = &mut ctx.accounts.cagnotte;
+        let admin_account = &ctx.accounts.admin_account;
 
-    // Vérifier si l'utilisateur est bien un administrateur
-    if !admin_account.admins.contains(ctx.accounts.user.key) {
-        return Err(ErrorCode::Unauthorized.into());
+        // Vérifier si l'utilisateur est bien un administrateur
+        if !admin_account.admins.contains(ctx.accounts.user.key) {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+
+        cagnotte.locked = false;
+        msg!(
+            "La cagnotte {} a été déverrouillée.",
+            String::from_utf8_lossy(&cagnotte.name)
+        );
+
+        Ok(())
     }
-    cagnotte.locked = true;
-    msg!("La cagnotte {} a été verrouillée.", String::from_utf8_lossy(&cagnotte.name));
-
-    Ok(())
-}
-
-pub fn unlock_cagnotte(ctx: Context<ManageCagnotteLock>) -> Result<()> {
-    let cagnotte = &mut ctx.accounts.cagnotte;
-    let admin_account = &ctx.accounts.admin_account;
-
-    // Vérifier si l'utilisateur est bien un administrateur
-    if !admin_account.admins.contains(ctx.accounts.user.key) {
-        return Err(ErrorCode::Unauthorized.into());
-    }
-    
-    
-
-    cagnotte.locked = false;
-    msg!("La cagnotte {} a été déverrouillée.", String::from_utf8_lossy(&cagnotte.name));
-
-    Ok(())
-}
 
 }
 
@@ -218,18 +222,19 @@ pub struct Contribute<'info> {
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-    #[account(mut)]
+    #[account(mut, close = user)] // Le compte de la cagnotte est fermé et les fonds restants vont à l'utilisateur
     pub cagnotte: Account<'info, Cagnotte>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
+
+
 #[derive(Accounts)]
 pub struct GetBalance<'info> {
     pub cagnotte: Account<'info, Cagnotte>,
 }
-
 
 #[derive(Accounts)]
 pub struct InitializeAdmin<'info> {
@@ -255,15 +260,13 @@ pub struct AdminManagement<'info> {
     pub system_program: Program<'info, System>,
 }
 
-
-
 #[derive(Accounts)]
 pub struct ManageCagnotteLock<'info> {
     #[account(mut)]
     pub cagnotte: Account<'info, Cagnotte>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub admin_account: Account<'info, AdminAccount>,  // Ajout du compte admin pour la vérification
+    pub admin_account: Account<'info, AdminAccount>, // Ajout du compte admin pour la vérification
 }
 
 #[account]
@@ -272,27 +275,27 @@ pub struct Cagnotte {
     pub name: Vec<u8>,
     pub amount: u64,
     pub locked: bool,
-    pub contributions: Vec<(Pubkey, u64)>,
+    pub contributions: Vec<Contributions>,
 }
-
-//compte Contribution pour suivre l'avancement de chaque user dans chaque cagnotte 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct Contributions {
+    pub user: Pubkey,
+    pub amount: u64,
+}
+//compte Contribution pour suivre l'avancement de chaque user dans chaque cagnotte
 #[account]
 pub struct Contribution {
     pub user: Pubkey,
     pub amount: u64,
-    
-    
 }
 
-//compte  des admins 
+//compte  des admins
 #[account]
 pub struct AdminAccount {
     pub admins: Vec<Pubkey>, // Liste des admins
 }
 
-
-
-//gestion des erreurs 
+//gestion des erreurs
 #[error_code]
 pub enum ErrorCode {
     #[msg("You are not authorized to perform this action.")]
